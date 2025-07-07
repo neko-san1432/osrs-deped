5895bcb252521d7f3af17024c5e0a7f7ead099b7
neko-san <pyrrhuselcidgo@gmail.com>
2025-07-07 14:15:54 +0800

    .

--- db.js
+++ db.js
@@ -2,9 +2,9 @@
 import { createConnection } from 'mysql2';
 
 const connection = createConnection({
-  host: '192.168.0.133',
-  user: 'depeddavsur',
-  password: '@Deped2025',
+  host: '',
+  user: '',
+  password: '',
   database: 'your_database'
 });
 
